3b9f043f4cdb969213af94892f50b6af02a8594c
Helltraitor <helltraitor@hotmail.com>
2022-11-15 16:48:03 +0300

    Added package bar basic view

---
+++ src/components/PackageBar/PackageBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    flex flex-col
+    border="~ rounded gray-200 dark:gray-700"
+    class="container"
+    h-full
+    p-4
+  >
+    <div m-0>
+      <PackageSearch />
+    </div>
+    <div mt-a />
+  </div>
+</template>
+
+<style scoped lang="sass">
+div.container
+  min-width: fit-content
+</style>
