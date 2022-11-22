f6a5c49b887df51dc43d7fc50227db41bd06478a
Helltraitor <helltraitor@hotmail.com>
2022-11-23 02:00:45 +0300

    Added manifest toml struct

---
+++ src-tauri/src/deduty/package/manifest.rs
@@ -0,0 +1,28 @@
+use serde::Deserialize;
+
+pub enum PackageManifestVariants {
+    Premier
+}
+
+#[derive(Deserialize)]
+struct Manifest {
+    name: String
+}
+
+#[derive(Deserialize)]
+pub struct PackageManifest {
+    manifest: Manifest
+}
+
+impl PackageManifest {
+    pub fn as_string(&self) -> &String {
+        &self.manifest.name
+    }
+
+    pub fn to_enum(&self) -> Option<PackageManifestVariants> {
+        match self.manifest.name.as_str() {
+            "premier" => Some(PackageManifestVariants::Premier),
+            _ => None
+        }
+    }
+}
