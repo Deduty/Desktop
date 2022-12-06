822de67247d0abdb4ce15939c411fe7dfd5e4bb0
Helltraitor <helltraitor@hotmail.com>
2022-12-06 17:25:05 +0300

    Added link, shiki

--- src/components/PackageBar/PackageMenu/PackageMenuAbout/PackageMenuAboutContent.vue
+++ src/components/PackageBar/PackageMenu/PackageMenuAbout/PackageMenuAboutContent.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
+// MARKDOWN IMPORTS
 import MarkdownIt from 'markdown-it'
+import LinkAttributes from 'markdown-it-link-attributes'
+import Shiki from 'markdown-it-shiki'
 
 const { content, extension } = defineProps<{ content: string; extension: string }>()
 
+// MARKDOWN SETUP
 const ConfiguredMarkdownIt = MarkdownIt()
+  .use(Shiki, {
+    theme: {
+      light: 'vitesse-light',
+      dark: 'vitesse-dark',
+    },
+  })
+  .use(LinkAttributes, {
+    matcher: (link: string) => /^https?:\/\//.test(link),
+    attrs: {
+      target: '_blank',
+      rel: 'noopener',
+    },
+  })
 
 const RuntimeMarkdown = computed(() => {
   return {
