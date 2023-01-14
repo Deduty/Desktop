01f74befba461f63061d8c89c761d058561d0646
Helltraitor <helltraitor@hotmail.com>
2023-01-14 17:26:01 +0300

    Updated about form

--- src/components/PackageBar/PackageForm/PackageFormAbout.vue
+++ src/components/PackageBar/PackageForm/PackageFormAbout.vue
@@ -1,60 +1,44 @@
 <script setup lang="ts">
-import { invoke } from '@tauri-apps/api'
-import type { Ref } from 'vue'
 import type { DedutyPackage } from '~/composables/deduty'
+
+import Loading from '~/components/Loading.vue'
+import Reader from '~/components/Reader/Reader.vue'
 
 const { pkg } = defineProps<{ pkg: DedutyPackage | null }>()
 
-enum AboutScreen {
-  Content = 'Content',
-  Error = 'Error',
-  Loading = 'Loading',
-  Nothing = 'Nothing',
-}
+const ComponentInstance = shallowRef(Loading)
+const ComponentProperties = ref({})
 
-const aboutScreen = ref(AboutScreen.Loading)
-const contentExtension = ref('')
-const contentArray: Ref<Uint8Array> = ref(new Uint8Array())
 const errorMessage = ref('')
+onErrorCaptured((error) => {
+  errorMessage.value = error.message
+})
 
 watchEffect(async () => {
-  errorMessage.value = 'All values (include error message) are just flushed. If you see this text - frontend is little bit broken'
-  aboutScreen.value = AboutScreen.Loading
+  errorMessage.value = ''
+
+  ComponentProperties.value = {}
+  ComponentInstance.value = Loading
 
   if (pkg === null) {
-    errorMessage.value = 'Package not set for this window. You never suppose to see this'
-    aboutScreen.value = AboutScreen.Error
+    errorMessage.value = 'All values (include error message) are just flushed. If you see this text - frontend is little bit broken'
     return
   }
 
-  const files = pkg.files.files.filter(file => file.alias === 'about')
-  if (files.length === 0) {
-    aboutScreen.value = AboutScreen.Nothing
+  const candidates = pkg.files.files.filter(file => file.alias === 'about')
+  if (candidates.length === 0) {
+    errorMessage.value = 'NO ABOUT FILE'
     return
   }
-  if (files.length !== 1) {
+
+  if (candidates.length !== 1) {
     errorMessage.value = 'Package have several \'about\' alias. Expected only one for package representation'
-    aboutScreen.value = AboutScreen.Error
     return
   }
+  const [about] = candidates
 
-  const [about] = files
-
-  invoke('getPackageFile', { id: pkg.id, location: about.location })
-    .then((value: unknown) => {
-      if (!Array.isArray(value))
-        throw new Error(`Internal error: Application provides wrong type '${typeof value}' when 'string' was expected`)
-      return new Uint8Array(value)
-    })
-    .then((content: Uint8Array) => {
-      aboutScreen.value = AboutScreen.Content
-      contentExtension.value = about.extension
-      contentArray.value = content
-    })
-    .catch((error) => {
-      errorMessage.value = `Unable to get about file: ${error}`
-      aboutScreen.value = AboutScreen.Error
-    })
+  ComponentProperties.value = { reader: await about.createReader(), extension: about.extension }
+  ComponentInstance.value = Reader
 })
 </script>
 
@@ -63,30 +47,18 @@ watchEffect(async () => {
     h-full w-full
     m-0
   >
-    <!-- DEFAULT SCREENS -->
-    <Error v-if="aboutScreen === AboutScreen.Error" :message="errorMessage" />
-    <Loading v-if="aboutScreen === AboutScreen.Loading" />
-    <!-- CONTENT SCREEN -->
-    <Reader
-      v-if="aboutScreen === AboutScreen.Content"
-      :content="contentArray"
-      :extension="contentExtension"
-    />
-    <!-- NOTHING SCREEN (When no file with 'about' alias) -->
-    <div
-      v-if="aboutScreen === AboutScreen.Nothing"
-      h-full w-full
-      m-0
-      grid
-    >
-      <div
-        m-a
-        grid-cols-none
-        grid-rows-none
-        text-2xl
-      >
-        About is not represented
-      </div>
+    <div v-if="errorMessage">
+      <Error :message="errorMessage" />
     </div>
+    <Suspense v-else>
+      <!-- DONE - SHOW DYNAMIC COMPONENT -->
+      <template #default>
+        <component :is="ComponentInstance" v-bind="ComponentProperties" />
+      </template>
+      <!-- LOADING - SHOW LOADING ANIMATION -->
+      <template #fallback>
+        <Loading />
+      </template>
+    </Suspense>
   </div>
 </template>
