38e00b4482f15c954d38e11c14e4818932700c0d
Helltraitor <helltraitor@hotmail.com>
2023-01-24 16:35:21 +0300

    Public api is now available for scripts

--- src/pages/package/[package]/lection/[lection].vue
+++ src/pages/package/[package]/lection/[lection].vue
@@ -5,6 +5,9 @@ import { invoke } from '@tauri-apps/api'
 import { DedutyLection, type IDedutyLection } from '~/composables/deduty'
 import type { DedutyFileReader } from '~/composables/deduty/file/reader'
 
+import type { IDedutyApi } from '~/composables/deduty/api'
+import { DedutyLectionStorageApi, DedutyPackageStorageApi } from '~/composables/deduty/api/storage'
+
 const properties = defineProps<{ package: string; lection: string }>()
 
 interface ReaderFile {
@@ -27,9 +30,22 @@ invoke('getPackageLection', properties)
       })
     }
   })
+
+const Deduty: IDedutyApi = {
+  storage: {
+    lection: new DedutyLectionStorageApi(properties.package, properties.lection),
+    package: new DedutyPackageStorageApi(properties.lection),
+  },
+}
+
+document.Deduty = Deduty
 </script>
 
 <template>
+  <component is="script">
+    const Deduty = document.Deduty;
+    delete document.Deduty;
+  </component>
   <div
     h-full w-full
     p-2
