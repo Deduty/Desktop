fc7c925eccb01c2ee02b6d0a7e1f3fdc85efbf14
Helltraitor <helltraitor@hotmail.com>
2023-01-24 21:19:38 +0300

    Storage crate remade into package index

--- src-tauri/crates/deduty_package_index/src/active.rs
+++ src-tauri/crates/deduty_package_index/src/active.rs
@@ -3,10 +3,7 @@ use std::sync::Arc;
 
 use async_std::sync::RwLock;
 
-
-use uuid::Uuid;
-
-use deduty_package::package::traits::DedutyPackage;
+use deduty_package_traits::DedutyPackage;
 
 // Arc + RwLock - because can be used in several places at the same time
 // Option - because package can be sub while using by other
@@ -19,7 +16,7 @@ pub enum ActivePackage {
 pub type ActiveItem = Arc<RwLock<ActivePackage>>;
 
 pub struct ActiveStorage {
-    packages: RwLock<HashMap<Uuid, ActiveItem>>
+    packages: RwLock<HashMap<String, ActiveItem>>
 }
 
 impl ActiveStorage {
@@ -27,31 +24,31 @@ impl ActiveStorage {
         Self { packages: RwLock::new(HashMap::new()) }
     }
 
-    pub async fn add(&self, id: Uuid, package: Box<dyn DedutyPackage>) -> Option<ActiveItem> { 
+    pub async fn add(&self, id: String, package: Box<dyn DedutyPackage>) -> Option<ActiveItem> { 
         self.packages
             .write()
             .await
             .insert(id, Arc::new(RwLock::new(ActivePackage::Online(package))))
     }
 
-    pub async fn get(&self, id: &Uuid) -> Option<ActiveItem> {
+    pub async fn get(&self, id: &String) -> Option<ActiveItem> {
         self.packages
             .read()
             .await
-            .get(&id)
+            .get(id)
             .cloned()
     }
 
-    pub async fn list(&self) -> Vec<Uuid> {
+    pub async fn list(&self) -> Vec<String> {
         self.packages
             .read()
             .await
             .keys()
-            .copied()
+            .cloned()
             .collect()
     }
 
-    pub async fn sub(&self, id: &Uuid) -> Option<ActiveItem> {
+    pub async fn sub(&self, id: &String) -> Option<ActiveItem> {
         self.packages
             .write()
             .await
