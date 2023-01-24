549ad9aee93f5b978675a969e31f082a745dd4e2
Helltraitor <helltraitor@hotmail.com>
2023-01-24 16:34:36 +0300

    Create public storage api

---
+++ src/composables/deduty/api/storage/lection.ts
@@ -0,0 +1,16 @@
+import { invoke } from '@tauri-apps/api'
+import { DedutyStorageApi } from './abstract'
+import { ValueAsBoolean, ValueAsStringOrNull } from './utils'
+
+export class DedutyLectionStorageApi extends DedutyStorageApi {
+  constructor(private pkg: string, private lection: string) {
+    super(
+      (key: string) =>
+        invoke('lectionStorageGet', { package: this.pkg, lection: this.lection, key })
+          .then(ValueAsStringOrNull),
+      (key: string, value: string, replace: boolean) =>
+        invoke('lectionStorageSet', { package: this.pkg, lection: this.lection, key, value, replace })
+          .then(ValueAsBoolean),
+    )
+  }
+}
---
+++ src/composables/deduty/api/storage/package.ts
@@ -0,0 +1,16 @@
+import { invoke } from '@tauri-apps/api'
+import { DedutyStorageApi } from './abstract'
+import { ValueAsBoolean, ValueAsStringOrNull } from './utils'
+
+export class DedutyPackageStorageApi extends DedutyStorageApi {
+  constructor(private pkg: string) {
+    super(
+      (key: string) =>
+        invoke('packageStorageGet', { package: this.pkg, key })
+          .then(ValueAsStringOrNull),
+      (key: string, value: string, replace: boolean) =>
+        invoke('packageStorageSet', { package: this.pkg, key, value, replace })
+          .then(ValueAsBoolean),
+    )
+  }
+}
