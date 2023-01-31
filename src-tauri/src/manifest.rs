d71c379560d4be3a247a483d92574c6301a6e002
Helltraitor <helltraitor@hotmail.com>
2023-02-01 01:30:17 +0300

    Update manifest.rs

--- src-tauri/src/manifest.rs
+++ src-tauri/src/manifest.rs
@@ -1,13 +1,15 @@
+use std::fmt::Display;
+
 use serde::Deserialize;
 
 pub enum PackageManifestVariants {
     Premier
 }
 
-impl PackageManifestVariants {
-    pub fn to_string(&self) -> String {
+impl Display for PackageManifestVariants {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match &self {
-            PackageManifestVariants::Premier => "premier".to_string()
+            PackageManifestVariants::Premier => f.write_str("premier")
         }
     }
 }
