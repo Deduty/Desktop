8b06d1a283264585a493393f86c866748517f669
Helltraitor <helltraitor@hotmail.com>
2023-02-04 22:28:52 +0300

    Implemented update functional

--- src-tauri/crates/deduty_scheme_premier/src/service.rs
+++ src-tauri/crates/deduty_scheme_premier/src/service.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{ HashMap, HashSet };
 use std::sync::Arc;
 
 use async_std::path::{ Path, PathBuf };
@@ -10,11 +10,14 @@ use deduty_package_traits::DedutyPackage;
 use deduty_package_index::{
     IndexService,
     PackageAgent,
-    PackageStatus
+    PackageStatus,
+    FrontEndRequirement as FER,
+    FrontEndSerialization as FES
 };
 
 use xresult::{ XError, XReason, XResult };
 
+use crate::lection::PremierLection;
 use crate::package::PremierPackage;
 use crate::agent::PremierPackageAgent;
 
@@ -23,12 +26,19 @@ type SafePackageAgent = Arc<RwLock<Box<dyn PackageAgent>>>;
 
 pub struct PremierIndexService {
     packages: HashMap<String, SafePackageAgent>,
+    reqs: HashMap<String, FER>,
     root: PathBuf
 }
 
 impl PremierIndexService {
     pub fn new(root: PathBuf) -> Self {
-        Self { packages: HashMap::new(), root }
+        Self {
+            packages: HashMap::new(),
+            root,
+            reqs: HashMap::from([
+                ("path".to_string(), FER::DirectoryPath)
+            ])
+        }
     }
 }
 
@@ -111,15 +121,6 @@ impl IndexService for PremierIndexService {
         Ok(reasons)
     }
 
-    async fn add(&mut self, serialized: String) -> XResult<SafePackageAgent> {
-        let package = PremierPackage::load(PathBuf::from(serialized)).await?;
-        let package_id = package.id();
-        let agent = Box::new(PremierPackageAgent::new(Box::new(package) as Box<dyn DedutyPackage>)) as Box<dyn PackageAgent>;
-        let safe_agent = Arc::new(RwLock::new(agent));
-        self.packages.insert(package_id, safe_agent.clone());
-        Ok(safe_agent)
-    }
-
     async fn get(&self, id: &String) -> XResult<Option<SafePackageAgent>> {
         Ok(self.packages.get(id).cloned())
     }
@@ -160,8 +161,105 @@ impl IndexService for PremierIndexService {
         }
     }
 
-    async fn update(&mut self, _serialized: String, _id: &String) -> XReason {
-        // TODO: Implement it later
-        XError::from(("Premier index service error", "Update function is not implemented")).into()
+    async fn add(&mut self, serialized: HashMap<String, FES>) -> XResult<SafePackageAgent> {
+        let path: PathBuf = serialized
+            .get(&"path".to_string())
+            .ok_or(Box::new(XError::from(("Premier index service error", "Serialized object must have path entry"))))?
+            .unwrap(&FER::DirectoryPath)?
+            .into();
+
+        let package = PremierPackage::load(path).await?;
+        let package_id = package.id();
+        let agent = Box::new(PremierPackageAgent::new(Box::new(package) as Box<dyn DedutyPackage>)) as Box<dyn PackageAgent>;
+        let safe_agent = Arc::new(RwLock::new(agent));
+        self.packages.insert(package_id, safe_agent.clone());
+        Ok(safe_agent)
+    }
+
+    fn add_reqs(&self) -> &HashMap<String, FER> {
+        &self.reqs
+    }
+
+    async fn update(&mut self, serialized: HashMap<String, FES>, id: &String) -> XReason {
+        let path: PathBuf = serialized
+            .get(&"path".to_string())
+            .ok_or(Box::new(XError::from(("Premier index service error", "Serialized object must have path entry"))))?
+            .unwrap(&FER::DirectoryPath)?
+            .into();
+
+        let mut update = PremierPackage::load(path).await?;
+
+        let update_lections_names: Vec<_> = update.lections.iter().map(|lection| lection.meta().name()).collect();
+        let mut update_lections: HashMap<_, _> = update.lections.iter_mut().map(|lection| (lection.meta().name(), lection)).collect();
+        if update_lections.len() != update_lections_names.len() {
+            // Determine overlap name for error message
+            let mut update_names: HashSet<_> = update_lections.values().map(|lection| lection.meta().name()).collect();
+            for name in update_lections_names {
+                if !update_names.remove(&name) {
+                    return Err(XError::from(("Premier index service error", format!("Unable to update package that have same lection name `{name}`"))).into());
+                }
+            }
+
+            return Err(XError::from(("Premier index service error", "Critical error: While determine lection name overlap - no such lection exist, but hashmap of names have different size from all lections")).into());
+        }
+
+        // Note: Irrefutable let pattern required for keeping origin write guard
+        #[allow(irrefutable_let_patterns)]
+        if let origin =
+            self.packages
+                .get_mut(id)
+                .ok_or_else(|| Box::new(XError::from(("Premier index service error", format!("Package with `{id}` is not found")))))?
+                .write()
+                .await
+                .package_mut()
+                .to_option()
+                .ok_or_else(|| Box::new(XError::from(("Premier index service error", format!("Package with `{id}` is offline")))))?
+                .as_any_mut()
+                .downcast_mut::<PremierPackage>()
+                .ok_or_else(|| Box::new(XError::from(("Premier index service error", format!("Package with `{id}` has wrong internal type")))))?
+        {
+            // Check unique names
+            // Note: In case when several lections have the same name
+            //       further update is impossible
+            let origin_lections: HashMap<_, _> = origin.lections.iter().map(|lection| (lection.meta().name(), lection)).collect();
+            if origin_lections.len() != origin.lections.len() {
+                // Determine overlap name for error message
+                let mut origin_names: HashSet<_> = origin_lections.values().map(|lection| lection.meta().name()).collect();
+                for name in origin.lections.iter().map(|lection| lection.meta().name()) {
+                    if !origin_names.remove(&name) {
+                        return Err(XError::from(("Premier index service error", format!("Unable to update package that have same lection name `{name}`"))).into());
+                    }
+                }
+
+                return Err(XError::from(("Premier index service error", "Critical error: While determine lection name overlap - no such lection exist, but hashmap of names have different size from all lections")).into());
+            }
+
+            update.id = origin.id;
+
+            for (name, origin_lection) in origin_lections {
+                if let Some(update_lection) = update_lections.get_mut(&name) {
+                    match (
+                        origin_lection.as_any_ref().downcast_ref::<PremierLection>(),
+                        update_lection.as_any_mut().downcast_mut::<PremierLection>()
+                    ) {
+                        (Some(origin_lection), Some(update_lection)) => update_lection.id = origin_lection.id,
+                        (Some(_), None) => return Err(XError::from(("Premier index service error", format!("Update lection has wrong internal type in `{id}` package"))).into()),
+                        _ => return Err(XError::from(("Premier index service error", format!("Both lections has wrong internal types in `{id}` package"))).into())
+                    }
+                }
+            }
+        }
+
+        let agent = PremierPackageAgent::new(Box::new(update) as Box<dyn DedutyPackage>);
+        self.packages
+            .insert(id.clone(), Arc::new(RwLock::new(Box::new(agent) as Box<dyn PackageAgent>)))
+            .expect("Premier fatal error: Replaced package must exist because it was used right before")
+            .write()
+            .await
+            .offline()
+    }
+
+    fn update_reqs(&self) -> &HashMap<String, FER> {
+        &self.reqs
     }
 }
