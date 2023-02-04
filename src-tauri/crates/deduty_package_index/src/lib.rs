9c517db9bae0596180b0f2ff65c2a9fb8665c3ea
Helltraitor <helltraitor@hotmail.com>
2023-02-04 22:28:14 +0300

    Added basic requirements implementation
    This infrastructure must solve adding and updating problem. Package now can provide requirements that frontend must satisfy on add \ update package

--- src-tauri/crates/deduty_package_index/src/lib.rs
+++ src-tauri/crates/deduty_package_index/src/lib.rs
@@ -1,7 +1,9 @@
 mod agent;
 mod index;
 mod service;
+mod reqs;
 
 pub use agent::{ PackageAgent, PackageStatus };
 pub use index::DedutyPackageIndex;
+pub use reqs::{ FrontEndRequirement, FrontEndSerialization };
 pub use service::IndexService;
--- src-tauri/crates/deduty_package_index/src/service.rs
+++ src-tauri/crates/deduty_package_index/src/service.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::sync::Arc;
 
 use async_std::sync::RwLock;
@@ -6,6 +7,10 @@ use async_trait::async_trait;
 use xresult::{ XReason, XResult };
 
 use crate::agent::PackageAgent;
+use crate::reqs::{
+    FrontEndRequirement as FER,
+    FrontEndSerialization as FES
+};
 
 
 type SafePackageAgent = Arc<RwLock<Box<dyn PackageAgent>>>;
@@ -16,14 +21,14 @@ pub trait IndexService: Send + Sync {
     async fn load_all(&mut self) -> XResult<Vec<XReason>>;
     async fn save_all(&mut self) -> XResult<Vec<XReason>>;
 
-    async fn add(&mut self, serialized: String) -> XResult<SafePackageAgent>;
     async fn get(&self, id: &String) -> XResult<Option<SafePackageAgent>>;
     async fn has(&self, id: &String) -> XResult<bool>;
     async fn list(&self) -> XResult<Vec<String>>;
     async fn sub(&mut self, id: &String) -> XReason;
-    // TODO: Update accepts HashMap<String, FrontEndSerialization>
-    //       Where FES is String that served by update method.
-    //       Requirements gives HashMap<String, FrontEndRequirement>
-    //       Where FER is ZeroSizedType which can only be serialized.
-    async fn update(&mut self, serialized: String, id: &String) -> XReason;
+
+    async fn update(&mut self, serialized: HashMap<String, FES>, id: &String) -> XReason;
+    fn update_reqs(&self) -> &HashMap<String, FER>;
+
+    async fn add(&mut self, serialized: HashMap<String, FES>) -> XResult<SafePackageAgent>;
+    fn add_reqs(&self) -> &HashMap<String, FER>;
 }
