ac66024d0a9dbe88a3e28c73147c278247e9d2b4
Helltraitor <helltraitor@hotmail.com>
2023-02-05 05:05:38 +0300

    Update components.d.ts

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,18 +7,20 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    DirectoryPathRequirement: typeof import('./components/PackageRequirements/DirectoryPathRequirement.vue')['default']
     Error: typeof import('./components/Error.vue')['default']
     LectionItem: typeof import('./components/LectionList/LectionItem.vue')['default']
     LectionList: typeof import('./components/LectionList/LectionList.vue')['default']
     Loading: typeof import('./components/Loading.vue')['default']
-    LocalPackageAdd: typeof import('./components/PackageAdd/LocalPackageAdd.vue')['default']
     PackageAdd: typeof import('./components/PackageAdd/PackageAdd.vue')['default']
+    PackageAddSuspense: typeof import('./components/PackageAdd/PackageAddSuspense.vue')['default']
     PackageForm: typeof import('./components/PackageForm/PackageForm.vue')['default']
     PackageFormAbout: typeof import('./components/PackageForm/PackageFormAbout.vue')['default']
     PackageFormMenu: typeof import('./components/PackageForm/PackageFormMenu.vue')['default']
     PackageFormSettings: typeof import('./components/PackageForm/PackageFormSettings.vue')['default']
     PackageItem: typeof import('./components/PackageList/PackageItem.vue')['default']
     PackageList: typeof import('./components/PackageList/PackageList.vue')['default']
+    PackageRequirements: typeof import('./components/PackageRequirements/PackageRequirements.vue')['default']
     Reader: typeof import('./components/Reader/Reader.vue')['default']
     ReaderHtmlComponent: typeof import('./components/Reader/ReaderHtmlComponent.vue')['default']
     ReaderImageComponent: typeof import('./components/Reader/ReaderImageComponent.vue')['default']
