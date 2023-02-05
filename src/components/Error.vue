556be63ed54a044fa20a4fa72e5830c7958a4faf
Helltraitor <helltraitor@hotmail.com>
2023-02-05 16:47:50 +0300

    Update Error.vue

--- src/components/Error.vue
+++ src/components/Error.vue
@@ -7,7 +7,7 @@ const { message } = defineProps<{ message: string }>()
     h-full w-full
     align-middle
     justify-center
-
+    flex flex-grow
     border="~ rounded red-200 dark:red-700"
     m-0 p-4
   >
