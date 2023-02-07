6c4969691abd1a7bf6e656951654da218abe8ed5
Helltraitor <helltraitor@hotmail.com>
2023-02-07 09:01:54 +0300

    Added service manager as replacement for package index

--- src-tauri/src/managers/mod.rs
+++ src-tauri/src/managers/mod.rs
@@ -1,7 +1,9 @@
 mod web_storage;
 mod readers;
+mod services;
 mod settings;
 
 pub use web_storage::WebStorageManager;
 pub use readers::ReaderManager;
+pub use services::ServiceManager;
 pub use settings::Settings;
---
+++ src-tauri/src/managers/services.rs
@@ -0,0 +1,130 @@
+use std::collections::HashMap;
+use std::ops::Deref;
+use std::sync::Arc;
+
+use async_std::sync::RwLock;
+use xresult::{ XResult, XError, XReason };
+
+use deduty_package::{ DedutyPackage, DedutyLection, DedutyFile };
+use deduty_service::Service;
+
+
+type ServiceRef = Box<dyn Deref<Target = dyn Service>>;
+
+fn into_service_ref(service: Arc<dyn Service>) -> Box<dyn Deref<Target = dyn Service>> {
+    Box::new(service) as Box<dyn Deref<Target = dyn Service>>
+}
+
+
+enum ServiceEntry {
+    Some(ServiceRef),
+    None
+}
+
+impl ServiceEntry {
+    pub async fn with_package(&self, package: &str) -> XResult<Option<&dyn DedutyPackage>> {
+        match self {
+            Self::Some(service) => {
+                service.get(package).await
+            },
+            Self::None => Ok(None)
+        }
+    }
+
+    pub async fn with_lection(&self, package: &str, lection: &str) -> XResult<Option<&dyn DedutyLection>> {
+        match self {
+            Self::Some(service) => {
+                Ok(
+                    service
+                        .get(package)
+                        .await?
+                        .ok_or_else(|| XError::from(("Service error", format!("Package with id `{package}` not found at `{}`", service.id()))))?
+                        .lection(lection)
+                )
+            },
+            Self::None => Ok(None)
+        }
+    }
+
+    pub async fn with_file(&self, package: &str, lection: &str, file: &str) -> XResult<Option<&dyn DedutyFile>> {
+        match self {
+            Self::Some(service) => {
+                Ok(
+                    service
+                        .get(package)
+                        .await?
+                        .ok_or_else(|| XError::from(("Service error", format!("Package with id `{package}` not found at `{}`", service.id()))))?
+                        .lection(lection)
+                        .ok_or_else(|| XError::from(("Service error", format!("Lection with id `{lection}` not found at package with id `{package}` not found at `{}`", service.id()))))?
+                        .file(file)
+                        .await?
+                )
+            },
+            Self::None => Ok(None)
+        }
+    }
+}
+
+
+pub struct ServiceManager {
+    services: RwLock<HashMap<String, Arc<dyn Service>>>
+}
+
+impl ServiceManager {
+    pub fn new() -> Self {
+        Self { services: Default::default() }
+    }
+
+    pub async fn access(&self, service: &str) -> ServiceEntry {
+        match self.services.read().await.get(service) {
+            Some(service) => ServiceEntry::Some(Box::new(service.clone())),
+            None => ServiceEntry::None
+        }
+    }
+
+    pub async fn list(&self) -> impl Iterator<Item = ServiceRef> {
+        self.services
+            .read()
+            .await
+            .values()
+            .cloned()
+            .map(into_service_ref)
+            .collect::<Vec<_>>().into_iter()
+    }
+
+    pub async fn add(&self, service: Box<dyn Service>) -> XReason {
+        if self.services.read().await.contains_key(service.id()) {
+            return XError::from(("Service error", format!("Unable to add a new service with an existed id `{}`", service.id()))).into();
+        }
+
+        self.services
+            .write()
+            .await
+            .insert(service.id().clone(), service.into());
+        Ok(())
+    }
+
+    pub async fn has(&self, service: &str) -> bool {
+        self.services
+            .read()
+            .await
+            .contains_key(service)
+    }
+
+    pub async fn get(&self, service: &str) -> Option<ServiceRef> {
+        self.services
+            .read()
+            .await
+            .get(service)
+            .cloned()
+            .map(into_service_ref)
+    }
+
+    pub async fn sub(&self, service: &str) -> Option<ServiceRef> {
+        self.services
+            .write()
+            .await
+            .remove(service)
+            .map(into_service_ref)
+    }
+}
