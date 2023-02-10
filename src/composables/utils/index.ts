72a64c142361d6e71b4cb7bda22af5f774a27293
Helltraitor <helltraitor@hotmail.com>
2023-02-10 22:26:34 +0300

    Added utils

---
+++ src/composables/utils/index.ts
@@ -0,0 +1,1 @@
+export { updateValues } from './object'
---
+++ src/composables/utils/object.ts
@@ -0,0 +1,19 @@
+/**
+ * Updated target from values and returns its. Mutates target without new keys applied.
+ * Have basic type check via `typeof`
+ *
+ * @param target Object that will be updated
+ * @param values Object which data will be used for target update
+ * @returns target for handly using
+ */
+export const updateValues = <T extends object>(target: T, values: object): T => {
+  for (const [key, value] of Object.entries(values)) {
+    if (
+      Object.hasOwn(target, key)
+      && (typeof value === typeof (target as any)[key])
+    )
+      (target as any)[key] = value
+  }
+
+  return target
+}
