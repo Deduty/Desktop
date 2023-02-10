88b96732a40c1022627c2df0035db3c6ab72d820
Helltraitor <helltraitor@hotmail.com>
2023-02-10 22:28:48 +0300

    Update package.ts

--- src/store/package.ts
+++ src/store/package.ts
@@ -1,40 +1,87 @@
-import { invoke } from '@tauri-apps/api'
 import { acceptHMRUpdate, defineStore } from 'pinia'
 import type { Ref } from 'vue'
 
 import type { IDedutyPackage } from '~/composables/deduty'
 import { DedutyPackage } from '~/composables/deduty'
+import * as Commands from '~/composables/commands'
 
 export const usePackageStore = defineStore('DedutyPackage', () => {
-  const packages: Ref<DedutyPackage[]> = ref([])
+  const storedPackages: Ref<DedutyPackage[]> = ref([])
+  const indexedPackages: Ref<Map<string, Map<string, DedutyPackage>>> = ref(new Map())
 
-  async function include(pkg: DedutyPackage): Promise<void> {
-    packages.value.push(pkg)
+  async function include(pack: DedutyPackage): Promise<void> {
+    storedPackages.value.push(pack)
+
+    if (!indexedPackages.value.get(pack.service))
+      indexedPackages.value.set(pack.service, new Map())
+
+    indexedPackages.value.get(pack.service)!.set(pack.id, pack)
   }
 
-  async function exclude(pkg: DedutyPackage): Promise<void> {
-    packages.value = [...packages.value.filter(storedPackage => storedPackage.id !== pkg.id)]
+  async function exclude(pack: DedutyPackage): Promise<void> {
+    if (!indexedPackages.value.get(pack.service) || indexedPackages.value.get(pack.service)!.get(pack.id) !== undefined) {
+      console.warn('Package is not contained by frontend', pack)
+      return
+    }
+
+    // Must be contained according to check above
+    indexedPackages.value.get(pack.service)!.delete(pack.id)
+
+    const newStoredPackages: DedutyPackage[] = []
+    for (const packages of indexedPackages.value.values())
+      newStoredPackages.push(...packages.values())
+
+    storedPackages.value = newStoredPackages
   }
 
-  async function refresh(totally = false) {
-    if (totally)
-      packages.value = []
+  async function refresh(totally = false, services: string[] = []) {
+    if (totally && services.length !== 0)
+      throw new Error('Refresh cannot be applied on total store but for some services. This can leads to miss some packages')
 
-    const updated: Set<string> = new Set(await invoke('listPackages'))
-    packages.value
-      .map(pack => pack.id)
-      .forEach(updated.delete)
+    if (totally) {
+      storedPackages.value = []
+      indexedPackages.value.clear()
+    }
 
-    for (const uuid of updated) {
+    if (services.length === 0) {
+      const allServices = await Commands.listServices()
+
+      if (!Array.isArray(allServices))
+        throw new TypeError('TODO: Type error')
+
+      services.push(...allServices)
+    }
+
+    const updatedServices: Map<string, Set<string>> = new Map()
+    for (const service of services) {
       try {
-        const serialized: IDedutyPackage = await invoke('getPackage', { id: uuid })
-        if (!serialized)
-          continue
+        const servicePackages = await Commands.listPackages(service)
 
-        packages.value.push(DedutyPackage.fromOptions(serialized))
+        if (!Array.isArray(servicePackages))
+          throw new TypeError('TODO: Type error')
+
+        updatedServices.set(service, new Set(servicePackages))
       }
       catch (error) {
-        console.error(`Internal error: Unable to fetch Package '${uuid}' due to: ${error}`)
+        console.error(`Unable to receive packages ids from service\`${service}\`\n`, error)
+      }
+    }
+
+    for (const service of updatedServices.keys())
+      indexedPackages.value.set(service, new Map())
+
+    for (const [service, packages] of updatedServices) {
+      for (const pack of packages) {
+        Commands.getPackage(service, pack)
+          .then((packageOptions: IDedutyPackage) => {
+            const objectPackage = DedutyPackage.fromOptions(service, packageOptions)
+
+            indexedPackages.value.get(service)!.set(pack, objectPackage)
+            storedPackages.value.push(objectPackage)
+          })
+          .catch((error: any) => {
+            console.error(`Unable to receive package with id \`${pack}\` from service with id \`${service}\`:\n`, error)
+          })
       }
     }
   }
@@ -42,7 +89,7 @@ export const usePackageStore = defineStore('DedutyPackage', () => {
   refresh()
     .catch(error => console.error(`Internal error: Unable to init Frontend Package storage due to: ${error}`))
 
-  return { packages, include, exclude, refresh }
+  return { storedPackages, indexedPackages, include, exclude, refresh }
 })
 
 if (import.meta.hot)
