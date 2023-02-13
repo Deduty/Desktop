b8a74068870275378f8605eda8b8b2cdfbd0f557
Helltraitor <helltraitor@hotmail.com>
2023-02-13 18:55:25 +0300

    Update statefull.rs

--- src-tauri/crates/deduty_service/src/statefull.rs
+++ src-tauri/crates/deduty_service/src/statefull.rs
@@ -1,12 +1,15 @@
 use async_std::path::PathBuf;
 use async_trait::async_trait;
-use xresult::{ XReason, XResult };
+use xresult::XResult;
 
 /// ### Important
 /// Type that implements this trait must have internal mutability
 ///
 #[async_trait]
 pub trait StateFullService: Send + Sync {
-    async fn load_all(&self, root: &PathBuf) -> XResult<Box<dyn Iterator<Item = XReason> + Send>>;
-    async fn save_all(&self, root: &PathBuf) -> XResult<Box<dyn Iterator<Item = XReason> + Send>>;
+    async fn get_root(&self) -> XResult<Option<PathBuf>>;
+    async fn set_root(&self, root: PathBuf) -> XResult<Option<PathBuf>>;
+
+    async fn load_all(&self, root: &PathBuf) -> XResult<Box<dyn Iterator<Item = XResult<String>> + Send>>;
+    async fn save_all(&self, root: &PathBuf) -> XResult<Box<dyn Iterator<Item = XResult<String>> + Send>>;
 }
