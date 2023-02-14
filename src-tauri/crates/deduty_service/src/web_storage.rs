85a4e767ba3bb980aa92a0f2d312faf6820a258a
Helltraitor <helltraitor@hotmail.com>
2023-02-14 14:59:28 +0300

    Storage moved into web_storage
    Also added some

--- src-tauri/crates/deduty_service/src/web_storage.rs
+++ src-tauri/crates/deduty_service/src/web_storage.rs
@@ -4,6 +4,12 @@ use xresult::XResult;
 
 #[async_trait]
 pub trait WebStorageService: Send + Sync {
+    async fn export(&self, package: &str, path: &str) -> XResult<()>;
+
+    async fn import(&self, package: &str, path: &str) -> XResult<()>;
+
+    async fn clear(&self, package: &str) -> XResult<()>;
+
     async fn delete(&self, package: &str, lection: Option<&str>, key: &str) -> XResult<Option<String>>;
 
     async fn get(&self, package: &str, lection: Option<&str>, key: &str, fallback: Option<&str>) -> XResult<Option<String>>;
