a55dd4f5168cab87e9cf0faa8bf947dc2fb4df2a
Helltraitor <helltraitor@hotmail.com>
2023-02-18 14:45:33 +0300

    Fixed non existed lection on before store init

--- src/pages/services/[service]/packages/[package]/lections/[lection].vue
+++ src/pages/services/[service]/packages/[package]/lections/[lection].vue
@@ -1,21 +1,36 @@
 <script setup lang="ts">
+import type { Ref } from 'vue'
+import type { DedutyLection } from '~/composables/deduty'
+
 const properties = defineProps<{ service: string; package: string; lection: string }>()
 
 const packageStore = usePackageStore()
 
-const lectionObject = (
-  packageStore
-    .indexedPackages
-    .get(properties.service)
-    ?.get(properties.package)
-    ?.lections
-    .find(lectionObject => lectionObject.id === properties.lection))
+const errorMessage = ref('')
+onErrorCaptured((error) => {
+  errorMessage.value = error.message
+})
 
-const errorMessage = `Lection with id \`${properties.lection}\` not found. Probably service, package or lection is not exist.`
+const lectionObject: Ref<DedutyLection | undefined> = ref()
+
+onMounted(async () => {
+  await Promise.all([packageStore.initialRefreshPromise])
+  lectionObject.value = (
+    packageStore
+      .indexedPackages
+      .get(properties.service)
+      ?.get(properties.package)
+      ?.lections
+      .find(lectionObject => lectionObject.id === properties.lection)
+  )
+
+  if (!lectionObject.value)
+    errorMessage.value = `Lection with id \`${properties.lection}\` not found. Probably service, package or lection is not exist.`
+})
 </script>
 
 <template>
-  <div v-if="!lectionObject" flex-grow>
+  <div v-if="errorMessage" flex-grow>
     <Error :message="errorMessage" />
   </div>
   <LectionReader
@@ -25,4 +40,5 @@ const errorMessage = `Lection with id \`${properties.lection}\` not found. Proba
     :lection="lectionObject"
     :api-enabled="true"
   />
+  <Loading v-else />
 </template>
