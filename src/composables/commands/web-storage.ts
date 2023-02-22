b695c2a522922a6b206148172614da893ee82bc3
Helltraitor <helltraitor@hotmail.com>
2023-02-22 20:39:40 +0300

    Update web-storage.ts

--- src/composables/commands/web-storage.ts
+++ src/composables/commands/web-storage.ts
@@ -27,11 +27,11 @@ export const webStorageClear = (service: string, pack: string): Promise<void> =>
 export const webStorageDelete = (service: string, pack: string, lection: string | undefined, key: string): Promise<string | undefined> => {
   return invoke('webStorageDelete', { service, package: pack, lection, key })
     .then((value) => {
-      if (!value)
-        return undefined
-
       if (typeof value === 'string')
         return value
+
+      if (!value)
+        return undefined
 
       throw new TypeError(`Internal error: \`webStorageDelete\` has returned \`${value}\` but \`string\` was expected`)
     })
@@ -40,11 +40,11 @@ export const webStorageDelete = (service: string, pack: string, lection: string 
 export const webStorageGet = (service: string, pack: string, lection: string | undefined, key: string, fallback: string | undefined): Promise<string | undefined> => {
   return invoke('webStorageGet', { service, package: pack, lection, key, fallback })
     .then((value) => {
-      if (!value)
-        return undefined
-
       if (typeof value === 'string')
         return value
+
+      if (!value)
+        return undefined
 
       throw new TypeError(`Internal error: \`webStorageGet\` has returned \`${value}\` but \`string\` was expected`)
     })
@@ -53,11 +53,11 @@ export const webStorageGet = (service: string, pack: string, lection: string | u
 export const webStorageSet = (service: string, pack: string, lection: string | undefined, key: string, value: string, replaced: boolean): Promise<string | undefined> => {
   return invoke('webStorageSet', { service, package: pack, lection, key, value, replaced })
     .then((value) => {
-      if (!value)
-        return undefined
-
       if (typeof value === 'string')
         return value
+
+      if (!value)
+        return undefined
 
       throw new TypeError(`Internal error: \`webStorageSet\` has returned \`${value}\` but \`string\` was expected`)
     })
