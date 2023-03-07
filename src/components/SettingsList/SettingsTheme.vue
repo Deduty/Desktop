fcd995929c64c237fe7d7e2a36fe8addd2bc116b
Helltraitor <helltraitor@hotmail.com>
2023-03-07 18:10:44 +0300

    Added theme setting

---
+++ src/components/SettingsList/SettingsTheme.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import { isDark, preferredDark, toggleDark } from '~/composables/dark'
+
+const isAutoUseDark = ref(localStorage['vueuse-color-scheme'] === 'auto')
+
+const useDarkToggle = () => {
+  isAutoUseDark.value = false
+  toggleDark()
+}
+
+const useSystemThemeToggle = () => {
+  if (isAutoUseDark.value) {
+    localStorage['vueuse-color-scheme'] = isDark.value ? 'dark' : 'light'
+    isAutoUseDark.value = false
+  }
+  else {
+    localStorage['vueuse-color-scheme'] = 'auto'
+    toggleDark(preferredDark.value)
+    isAutoUseDark.value = true
+  }
+}
+</script>
+
+<template>
+  <div flex flex-col gap-2>
+    <div text-2xl>
+      Theme
+    </div>
+    <div
+      flex flex-row
+      p-2
+      border="~ rounded gray-200 dark:gray-700"
+    >
+      <div flex mr-a>
+        Change theme
+      </div>
+      <div @click="useDarkToggle()">
+        <div
+          v-if="!isDark"
+          flex flex-row
+          gap-2
+        >
+          <div>Day</div>
+          <div m-a i-carbon-sun />
+        </div>
+        <div
+          v-if="isDark"
+          flex flex-row
+          gap-2
+        >
+          <div>Night</div>
+          <div m-a i-carbon-moon />
+        </div>
+      </div>
+    </div>
+    <div
+      flex flex-row
+      p-2
+      border="~ rounded gray-200 dark:gray-700"
+    >
+      <div flex mr-a>
+        Use system settings
+      </div>
+      <div flex>
+        <input
+          m-a
+          type="checkbox"
+          :checked="isAutoUseDark"
+          @click="useSystemThemeToggle()"
+        >
+      </div>
+    </div>
+  </div>
+</template>
